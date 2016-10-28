7d863832dd496bf03950585a1bcdddafe1896e49
Clemens Damke <admin@cortys.de>
2016-10-28 18:16:16 +0200

    moved plugin/Plugin and plugin/FsPlugin => plugin/abstract/*
    mixins are now separated from instanciable Plugin types.
    
    + renamed the "assignGraph" stage to "setDerivedData" to keep domain specifics out of the application scheduler configuration.
    + "validatePlugin" now is a critical stage. To keep the recursive Plugin#assign call (that occurs as part of the Plugin#update call during validation of external FsPlugins) working, non-validating plugin assign jobs are now canceled using the stageManager's cancel feature instead of throwing like before.

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
 module.exports = {
 	taskManager: require("./taskManager")(),
 	stageManager: require("./stageManager")({
-		stages: ["setMetadata", "initializeDatabase", "assignGraph", "validatePlugin", "activateRunner"],
-		nonCriticalStages: ["validatePlugin", "activateRunner"]
+		stages: [
+			"setMetadata", // Set all data that can be directly derived from the preset.
+			"initializeDatabase", // Build indices and check constraints.
+			"setDerivedData", // Set all data that can be derived from the preset & all other instances.
+
+			"validatePlugin", // If instanceof Plugin: Validate the correctness of the Plugin installation.
+			"activateRunner" // If instanceof Runner: Activate Runner if it is in autostart mode.
+		],
+		// If validation of a Plugin or activation of a Runner fails, do not cancel all other stages:
+		nonCriticalStages: ["activateRunner"]
 	})
 };
